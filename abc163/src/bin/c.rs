c470ef421e7c0375a6ce5d8c8356f987575bebfc
taemath <hirotaka081630@gmail.com>
2022-09-17 17:42:14 +0900

    feat(163c): for

--- abc163/src/bin/c.rs
+++ abc163/src/bin/c.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+      n: usize,
+      s_vec: [usize; n-1],
+    }
+    let mut counter = vec![0; n];
+
+    for s in s_vec.iter() {
+        counter[s - 1] += 1;
+    }
+
+    for m in counter {
+        println!("{}", m);
+    }
 }
