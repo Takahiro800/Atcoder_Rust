524015f7bfca35565dffa07a9f3f5ee8e09737a7
taemath <hirotaka081630@gmail.com>
2022-09-17 17:41:55 +0900

    feat(163b): iter().sum::<i32>()

--- abc163/src/bin/b.rs
+++ abc163/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+      n: i32,
+      m: i32,
+      a: [i32; m],
+    };
+
+    let sum_a = a.iter().sum::<i32>();
+    if n >= sum_a {
+        println!("{}", n - sum_a);
+    } else {
+        println!("-1");
+    }
 }
