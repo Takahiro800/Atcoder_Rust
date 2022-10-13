ead447b308ed07fe92a11ac98c663343462df85e
taemath <hirotaka081630@gmail.com>
2022-10-13 22:08:41 +0900

    feat(tessoku-book): b08 Counting Points
    二次元の累積和

---
+++ tessoku-book/src/bin/b08.rs
@@ -0,0 +1,35 @@
+use proconio::input;
+
+fn main() {
+    input! {
+      n: usize,
+      xy: [(usize, usize); n],
+      q: usize,
+      abcd: [(usize, usize, usize, usize); q]
+    }
+
+    let max_size: usize = 1500;
+
+    let mut count = vec![vec![0 as usize; max_size + 1]; max_size + 1];
+
+    for (x, y) in xy {
+        count[y][x] += 1;
+    }
+
+    for i in 0..max_size {
+        for j in 0..max_size {
+            count[i + 1][j + 1] += count[i + 1][j];
+        }
+    }
+
+    for j in 1..=max_size {
+        for i in 0..max_size {
+            count[i + 1][j] += count[i][j];
+        }
+    }
+
+    for (a, b, c, d) in abcd {
+        let ans = count[d][c] + count[b - 1][a - 1] - count[d][a - 1] - count[b - 1][c];
+        println!("{}", ans);
+    }
+}
