ba63b4e51ff3bb62d24bf642e4f0ab9a3e48f4ae
taemath <hirotaka081630@gmail.com>
2022-10-15 00:00:33 +0900

    feat(tessoku-book): a10 Resort Hotel
    チャレンジ問題

---
+++ tessoku-book/src/bin/a10.rs
@@ -0,0 +1,30 @@
+use proconio::input;
+use std::cmp;
+
+fn main() {
+    input! {
+      n: usize,
+      a: [usize; n],
+      d: usize,
+      lr: [(usize, usize); d]
+    }
+
+    let mut p = vec![0; n + 1];
+    p[1] = a[0];
+    for i in 1..n {
+        p[i + 1] = cmp::max(p[i], a[i])
+    }
+
+    let mut q = vec![0; n + 1];
+    q[n] = a[n - 1];
+    for i in (1..n).rev() {
+        q[i] = cmp::max(q[i + 1], a[i - 1]);
+    }
+
+    for i in 0..d {
+        let left_max = p[lr[i].0 - 1];
+        let right_max = q[lr[i].1 + 1];
+
+        println!("{}", cmp::max(left_max, right_max));
+    }
+}
