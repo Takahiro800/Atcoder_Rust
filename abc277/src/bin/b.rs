512c3bef0cf3154fb57f87c723a49d644e201273
taemath <hirotaka081630@gmail.com>
2022-11-16 06:51:40 +0900

    feat(abc277): b.rs char, String, &str

--- abc277/src/bin/b.rs
+++ abc277/src/bin/b.rs
@@ -1,5 +1,36 @@
+#![allow(non_snake_case)]
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        mut S: [String; N]
+    };
+    let mut T = S.clone();
+
+    let HDCS: Vec<char> = "HDCS".chars().collect();
+    let num: Vec<char> = "A23456789TJQK".chars().collect();
+    for s in S {
+        let ch1 = s.chars().next().unwrap();
+        let ch2 = s.chars().nth(1).unwrap();
+
+        // if !["H", "D", "C", "S"].contains(ch1) {
+        if !HDCS.contains(&ch1) {
+            println!("No");
+            return;
+        }
+
+        if !num.contains(&ch2) {
+            println!("No");
+            return;
+        }
+    }
+
+    T.sort();
+    T.dedup();
+    if T.len() != N {
+        println!("No");
+        return;
+    }
+    println!("Yes");
 }
