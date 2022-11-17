28ed7718a4882d97b20ef893704e6c09dc675f4c
taemath <hirotaka081630@gmail.com>
2022-11-17 10:36:18 +0900

    feat(276/a): chars

--- abc276/src/bin/a.rs
+++ abc276/src/bin/a.rs
@@ -1,5 +1,18 @@
 use proconio::input;
+use proconio::marker::Chars;
 
+#[allow(non_snake_case)]
 fn main() {
-    unimplemented!();
+    input! {
+        S: Chars,
+    };
+
+    let n = S.len();
+    for i in (0..n).rev() {
+        if S[i] == 'a' {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+    println!("-1");
 }
