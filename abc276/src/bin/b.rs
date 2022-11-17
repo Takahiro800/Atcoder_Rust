08ebedf58ab2f595693de565440cd9689efd73f1
taemath <hirotaka081630@gmail.com>
2022-11-18 07:13:31 +0900

    feat(abc276): b BTreeSet

--- abc276/src/bin/b.rs
+++ abc276/src/bin/b.rs
@@ -1,5 +1,26 @@
 use proconio::input;
+use std::collections::BTreeSet;
 
+#[allow(non_snake_case)]
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        AB: [[usize; 2]; M]
+    };
+
+    let mut town = vec![BTreeSet::new(); N];
+
+    for e in AB.iter() {
+        town[e[0] - 1].insert(e[1]);
+        town[e[1] - 1].insert(e[0]);
+    }
+
+    for e in town.iter_mut() {
+        print!("{}", e.len());
+        for j in e.iter() {
+            print!(" {}", j);
+        }
+        print!("\n");
+    }
 }
