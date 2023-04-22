631d8c9a35eea0f2fcc04a59cbe1119c534e1b3c
taemath <hirotaka081630@gmail.com>
2023-04-22 22:36:09 +0900

    feat(abc082b): 文字列の辞書順

--- abc082/src/bin/b.rs
+++ abc082/src/bin/b.rs
@@ -1,5 +1,23 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        s: String,
+        t: String,
+    };
+
+    let mut a: Vec<char> = s.chars().collect();
+    let mut b: Vec<char> = t.chars().collect();
+    a.sort();
+    b.sort();
+    b.reverse();
+
+    let sorted_s: String = a.into_iter().collect();
+    let sorted_t: String = b.into_iter().collect();
+
+    if sorted_s < sorted_t {
+        println!("{}", "Yes");
+    } else {
+        println!("{}", "No");
+    }
 }
