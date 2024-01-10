b8acd6c71f32acf0c6c801cef90765fbe1fcbc93
taemath <hirotaka081630@gmail.com>
2024-01-11 06:10:22 +0900

    feat(abc 334): dの解き直し
    解説を見ずに解説と同様の解き方ができた

--- abc334/src/bin/d.rs
+++ abc334/src/bin/d.rs
@@ -2,5 +2,47 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        Q: usize,
+        mut R: [usize;N]
+    };
+
+    R.sort_unstable();
+    let mut r = vec![R[0]];
+
+    for i in 1..R.len() {
+        if let Some(last) = r.last() {
+            let num = last + R[i];
+            r.push(num);
+        }
+    }
+
+    for _ in 0..Q {
+        input! {q: usize}
+        let ans = binary_search(&r, q);
+        println!("{}", ans + 1)
+    }
+}
+
+fn binary_search(R: &Vec<usize>, n: usize) -> isize {
+    if R[0] > n {
+        return -1;
+    }
+
+    let mut l = 0;
+    let mut r = R.len();
+    let mut mid;
+
+    while l + 1 < r {
+        mid = (l + r) / 2;
+
+        if R[mid] <= n {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+
+    l as isize
 }
