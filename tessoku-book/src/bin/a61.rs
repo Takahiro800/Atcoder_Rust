d892be5a1f6f71c9a74adf86e983ead56ee56202
taemath <hirotaka081630@gmail.com>
2024-01-11 09:29:40 +0900

    feat(tessoku): a61 グラフ構造の作成

--- tessoku-book/src/bin/a61.rs
+++ tessoku-book/src/bin/a61.rs
@@ -1,5 +1,28 @@
+#![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        AB: [(usize, usize);M]
+    }
+
+    let mut G = vec![vec![]; N + 1];
+
+    for (a, b) in AB {
+        G[a].push(b);
+        G[b].push(a);
+    }
+
+    for (i, g) in G.iter().enumerate().skip(1) {
+        let s = g
+            .iter()
+            .sorted_unstable()
+            .map(|n| n.to_string())
+            .collect::<Vec<_>>()
+            .join(", ");
+        println!("{}: {{{}}}", i, s)
+    }
 }
