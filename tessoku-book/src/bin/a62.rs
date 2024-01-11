8b30c89a1447f97dd620e66231978729f802bcc8
taemath <hirotaka081630@gmail.com>
2024-01-11 09:29:54 +0900

    feat(tessoku): a62 DFSの実装

--- tessoku-book/src/bin/a62.rs
+++ tessoku-book/src/bin/a62.rs
@@ -1,5 +1,38 @@
+#![allow(non_snake_case)]
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        AB: [(usize, usize);M]
+    }
+    let mut glaph: Vec<Vec<usize>> = vec![vec![]; N + 1];
+    let mut visited: Vec<_> = vec![false; N + 1];
+    visited[0] = true;
+
+    for (a, b) in &AB {
+        glaph[*a].push(*b);
+        glaph[*b].push(*a);
+    }
+
+    dfs(&glaph, &mut visited, 1);
+
+    let ans: &str = if visited.iter().all(|&b| b) {
+        "The graph is connected."
+    } else {
+        "The graph is not connected."
+    };
+
+    println!("{}", ans)
+}
+
+fn dfs(glaph: &Vec<Vec<usize>>, visited: &mut Vec<bool>, pos: usize) {
+    visited[pos] = true;
+
+    for &node in &glaph[pos] {
+        if !visited[node] {
+            dfs(glaph, visited, node)
+        }
+    }
 }
