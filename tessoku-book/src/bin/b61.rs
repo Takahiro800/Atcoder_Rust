5278f3232a9764bfc943e35a23e35e4bdbfd41e5
taemath <hirotaka081630@gmail.com>
2024-01-16 09:40:32 +0900

    feat(tessoku): b61 グラフの作成

--- tessoku-book/src/bin/b61.rs
+++ tessoku-book/src/bin/b61.rs
@@ -1,5 +1,29 @@
+#![allow(non_snake_case)]
 use proconio::input;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        AB: [(usize, usize); M]
+    }
+
+    let mut grapth = vec![vec![]; N + 1];
+
+    for (a, b) in AB {
+        grapth[a].push(b);
+        grapth[b].push(a);
+    }
+
+    let mut count = 0;
+    let mut ans = 0;
+
+    for (i, g) in grapth.iter().enumerate() {
+        if g.len() > count {
+            count = g.len();
+            ans = i;
+        }
+    }
+
+    println!("{}", ans)
 }
