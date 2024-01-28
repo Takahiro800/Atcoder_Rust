4446f7bdd5b9a498cc6ed4e9dd85a1405e5a2b0d
taemath <hirotaka081630@gmail.com>
2024-01-28 09:59:54 +0900

    feat(abc318): solve C

--- abc318/src/bin/c.rs
+++ abc318/src/bin/c.rs
@@ -1,8 +1,30 @@
 #![allow(non_snake_case)]
-use itertools::*;
 use proconio::input;
-use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        D: usize,
+        P: usize,
+        mut F: [usize;N]
+    };
+
+    F.sort_unstable();
+    F.reverse();
+    let mut count = 0;
+
+    for chunk in F.chunks(D) {
+        if chunk.iter().sum::<usize>() > P {
+            count += 1;
+        } else {
+            break;
+        }
+    }
+
+    let ans = if D * count < N {
+        F[D * count..].iter().sum::<usize>() + count * P
+    } else {
+        count * P
+    };
+    println!("{}", ans);
 }
