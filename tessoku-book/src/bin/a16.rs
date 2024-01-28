23de153d480f7e365185ef60da8308432036e6cb
taemath <hirotaka081630@gmail.com>
2024-01-29 04:57:23 +0900

    feat(tessoku): a16 DPの基本

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,27 +1,18 @@
 use proconio::input;
-use std::{cmp, collections::VecDeque};
 
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        n: usize,
-        mut A: [usize; n-1],
-        B: [usize; n-2]
+        N: usize,
+        A: [usize;N-1],
+        B: [usize; N-2]
     }
 
-    let mut C: VecDeque<usize> = VecDeque::from(A);
-    C.push_front(0);
+    let mut dp = vec![0; N];
+    dp[1] = A[0];
 
-    let mut D: VecDeque<usize> = VecDeque::from(B);
-    D.push_front(0);
-    D.push_front(0);
-
-    let mut dp: Vec<usize> = vec![0; n];
-    dp[0] = 0;
-    dp[1] = C[1];
-
-    for i in 2..n {
-        dp[i] = cmp::min(dp[i - 1] + C[i], dp[i - 2] + D[i]);
+    for i in 2..N {
+        dp[i] = (dp[i - 1] + A[i - 1]).min(dp[i - 2] + B[i - 2]);
     }
-    println!("{}", dp[n - 1])
+    println!("{}", dp[N - 1]);
 }
