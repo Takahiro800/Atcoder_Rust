11a6a1e9f1c320297a8e0bfe017e8838c97fe4c5
taemath <hirotaka081630@gmail.com>
2024-01-29 04:57:38 +0900

    feat(tessoku): a17 DPの復元

--- tessoku-book/src/bin/a17.rs
+++ tessoku-book/src/bin/a17.rs
@@ -1,55 +1,34 @@
-use std::cmp;
-use std::collections::VecDeque;
-
 use itertools::Itertools;
 use proconio::input;
 
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        n: usize,
-        mut A: [usize; n-1],
-        B: [usize; n-2]
+        N: usize,
+        A: [usize; N-1],
+        B: [usize; N-2]
     }
 
-    let mut C: VecDeque<usize> = VecDeque::from(A);
-    C.push_front(0);
-    C.push_front(0);
+    let mut dp = vec![0; N];
+    let mut prev = vec![0; N];
+    dp[1] = A[0];
 
-    let mut D: VecDeque<usize> = VecDeque::from(B);
-    D.push_front(0);
-    D.push_front(0);
-    D.push_front(0);
-
-    // 動的計画法
-    let mut dp: Vec<usize> = vec![0; n + 1];
-    dp[0] = 0;
-    dp[1] = 0;
-    dp[2] = C[2];
-
-    for i in 3..=n {
-        dp[i] = cmp::min(dp[i - 1] + C[i], dp[i - 2] + D[i]);
+    for i in 2..N {
+        let w1 = dp[i - 1] + A[i - 1];
+        let w2 = dp[i - 2] + B[i - 2];
+        dp[i] = w1.min(w2);
+        prev[i] = if w1 < w2 { i - 1 } else { i - 2 };
     }
 
-    let mut ans: VecDeque<usize> = VecDeque::new();
-
-    // 復元する
-    let mut current_place: usize = n;
-
-    while current_place > 0 {
-        ans.push_front(current_place);
-
-        if current_place == 1 {
-            break;
-        }
-
-        if dp[current_place] == dp[current_place - 1] + C[current_place] {
-            current_place -= 1;
-        } else {
-            current_place -= 2;
-        }
+    let mut cur = N - 1;
+    let mut path = Vec::new();
+    while cur != 0 {
+        path.push(cur);
+        cur = prev[cur];
     }
+    path.push(cur);
+    path.reverse();
 
-    println!("{}", ans.len());
-    println!("{}", ans.iter().join(" "));
+    println!("{}", path.len());
+    println!("{}", path.iter().map(|i| i + 1).join(" "))
 }
