c6e0d801c2e26045de1038f1b82b8abe9c11e68c
taemath <hirotaka081630@gmail.com>
2024-01-30 09:22:50 +0900

    feat(abc258c): solve it !

--- abc258/src/bin/c.rs
+++ abc258/src/bin/c.rs
@@ -1,8 +1,27 @@
 #![allow(non_snake_case)]
 // use itertools::*;
-use proconio::input;
+use proconio::{input, marker::Chars};
 // use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        Q: usize,
+        S: Chars
+    };
+
+    let mut count = 0;
+
+    for _ in 0..Q {
+        input! { q: usize }
+        input! { mut i: usize }
+        match q {
+            1 => count += i,
+            2 => {
+                i = (i + N - (count % N) - 1) % N;
+                println!("{}", S[i])
+            }
+            _ => unreachable!(),
+        }
+    }
 }
