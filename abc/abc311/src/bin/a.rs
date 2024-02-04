9e30df14ec4f5519a4fd4779731ba7d1caf05481
taemath <hirotaka081630@gmail.com>
2024-02-04 16:33:34 +0900

    feat(abc311): solve A

--- abc/abc311/src/bin/a.rs
+++ abc/abc311/src/bin/a.rs
@@ -1,8 +1,22 @@
 #![allow(non_snake_case)]
 // use itertools::*;
-use proconio::input;
+use proconio::{input, marker::Bytes};
 // use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        _N: usize,
+        S: Bytes
+    };
+
+    let mut set = [false; 3];
+    for (i, s) in S.iter().enumerate() {
+        let b = s - b'A';
+        set[b as usize] = true;
+
+        if set.iter().all(|&bool| bool) {
+            println!("{}", i + 1);
+            break;
+        }
+    }
 }
