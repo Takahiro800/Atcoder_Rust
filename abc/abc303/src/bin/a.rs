41a0bf3e6fc11a0165a9f8b203b194e8f761eea3
taemath <hirotaka081630@gmail.com>
2024-02-05 17:56:02 +0900

    feat(abc303a): solve

--- abc/abc303/src/bin/a.rs
+++ abc/abc303/src/bin/a.rs
@@ -1,8 +1,24 @@
 #![allow(non_snake_case)]
 // use itertools::*;
-use proconio::input;
+use proconio::{input, marker::Bytes};
 // use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        _N: usize,
+        S: Bytes,
+        T: Bytes
+    };
+
+    let mut ans = true;
+
+    for (&s, &t) in S.iter().zip(&T) {
+        let mut temp = s == t;
+        for &(a, b) in [(b'1', b'l'), (b'0', b'o')].iter() {
+            temp |= (s, t) == (a, b) || (s, t) == (b, a);
+        }
+        ans &= temp;
+    }
+
+    println!("{}", if ans { "Yes" } else { "No" });
 }
