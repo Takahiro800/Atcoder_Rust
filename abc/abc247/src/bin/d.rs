5a59fd6ed8600fa6ecf2d87c9c877d8680e996ea
taemath <hirotaka081630@gmail.com>
2024-02-07 06:51:00 +0900

    refactor(abc247d): refactor match!

--- abc/abc247/src/bin/d.rs
+++ abc/abc247/src/bin/d.rs
@@ -1,8 +1,44 @@
 #![allow(non_snake_case)]
-// use itertools::*;
+use std::collections::VecDeque;
+
 use proconio::input;
-// use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        Q: usize,
+    };
+
+    let mut que = VecDeque::new();
+    for _ in 0..Q {
+        input! {q: usize}
+
+        match q {
+            1 => {
+                input! { x: usize, c: usize };
+                que.push_back((c, x))
+            }
+            2 => {
+                input! { mut count: usize };
+                let mut sum = 0;
+                while count > 0 {
+                    if let Some((mut c, x)) = que.pop_front() {
+                        match count.cmp(&c) {
+                            std::cmp::Ordering::Less => {
+                                sum += count * x;
+                                c -= count;
+                                count = 0;
+                                que.push_front((c, x))
+                            }
+                            _ => {
+                                sum += c * x;
+                                count -= c;
+                            }
+                        }
+                    }
+                }
+                println!("{}", sum);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
