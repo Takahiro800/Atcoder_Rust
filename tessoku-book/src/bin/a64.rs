001a4b220ebe07f66ce8f962b31810b63bed5689
taemath <hirotaka081630@gmail.com>
2024-02-11 15:57:11 +0900

    feat(tessoku a64): 初めてのダイクストラ

--- tessoku-book/src/bin/a64.rs
+++ tessoku-book/src/bin/a64.rs
@@ -1,5 +1,43 @@
-use proconio::input;
+#![allow(non_snake_case)]
+use proconio::{input, marker::Usize1};
+use std::collections::BinaryHeap;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        ABC: [(Usize1, Usize1, isize);M]
+    }
+
+    let mut g = vec![vec![]; N];
+
+    for (a, b, c) in ABC {
+        g[a].push((b, c));
+        g[b].push((a, c));
+    }
+
+    let mut dp = vec![std::isize::MAX; N];
+    dp[0] = 0;
+    let mut heap = BinaryHeap::new();
+
+    heap.push((0, 0));
+
+    while let Some((d, p)) = heap.pop() {
+        let d = -d;
+
+        if d > dp[p] {
+            continue;
+        }
+
+        for (i, dis) in &g[p] {
+            if d + dis < dp[*i] {
+                dp[*i] = d + dis;
+                heap.push((-dp[*i], *i));
+            }
+        }
+    }
+
+    for a in dp {
+        println!("{}", if a == std::isize::MAX { -1 } else { a });
+    }
 }
