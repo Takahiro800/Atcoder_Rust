6d2d7352597df98cb0081f9289048c81fd3cad64
taemath <hirotaka081630@gmail.com>
2024-02-12 04:42:58 +0900

    feat(abc318b): solve it!

--- abc/abc318/src/bin/b.rs
+++ abc/abc318/src/bin/b.rs
@@ -1,8 +1,30 @@
 #![allow(non_snake_case)]
-use itertools::*;
+// use itertools::*;
 use proconio::input;
-use superslice::*;
+// use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+    };
+
+    let mut D = vec![vec![false; 100]; 100];
+
+    for _ in 0..N {
+        input! {
+            a: usize,
+            b: usize,
+            c: usize,
+            d: usize
+        }
+
+        for x in c..d {
+            for y in a..b {
+                D[y][x] = true;
+            }
+        }
+    }
+
+    let ans = D.iter().flatten().filter(|&p| *p).count();
+    println!("{}", ans);
 }
