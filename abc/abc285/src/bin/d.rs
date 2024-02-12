a420ed7b7a54aea6c67cb0621c8a5300affe4b25
taemath <hirotaka081630@gmail.com>
2024-02-13 06:31:15 +0900

    feat(abc285): 閉路の有無、UnionFind木

---
+++ abc/abc285/src/bin/d.rs
@@ -0,0 +1,96 @@
+#![allow(non_snake_case)]
+use std::collections::HashMap;
+
+use proconio::input;
+
+fn main() {
+    input! {
+        N: usize,
+        ST: [(String, String); N]
+    };
+
+    let mut x = vec![];
+
+    for (s, t) in &ST {
+        x.push(s.as_str());
+        x.push(t.as_str());
+    }
+    x.sort();
+    x.dedup();
+
+    let mp: HashMap<_, _> = x.iter().enumerate().map(|(i, s)| (s, i)).collect();
+    let mut uf = DSU::new(x.len());
+
+    let mut ans = true;
+
+    for (s, t) in &ST {
+        if uf.unite(mp[&s.as_str()], mp[&t.as_str()]).is_none() {
+            ans = false;
+            break;
+        }
+    }
+    println!("{}", if ans { "Yes" } else { "No" });
+}
+
+//---------- begin union_find ----------
+pub struct DSU {
+    p: Vec<i32>,
+}
+impl DSU {
+    pub fn new(n: usize) -> DSU {
+        assert!(n < std::i32::MAX as usize);
+        DSU { p: vec![-1; n] }
+    }
+    pub fn init(&mut self) {
+        self.p.iter_mut().for_each(|p| *p = -1);
+    }
+    pub fn root(&self, mut x: usize) -> usize {
+        assert!(x < self.p.len());
+        while self.p[x] >= 0 {
+            x = self.p[x] as usize;
+        }
+        x
+    }
+    pub fn same(&self, x: usize, y: usize) -> bool {
+        assert!(x < self.p.len() && y < self.p.len());
+        self.root(x) == self.root(y)
+    }
+    pub fn unite(&mut self, x: usize, y: usize) -> Option<(usize, usize)> {
+        assert!(x < self.p.len() && y < self.p.len());
+        let mut x = self.root(x);
+        let mut y = self.root(y);
+        if x == y {
+            return None;
+        }
+        if self.p[x] > self.p[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.p[x] += self.p[y];
+        self.p[y] = x as i32;
+        Some((x, y))
+    }
+    pub fn parent(&self, x: usize) -> Option<usize> {
+        assert!(x < self.p.len());
+        let p = self.p[x];
+        if p >= 0 {
+            Some(p as usize)
+        } else {
+            None
+        }
+    }
+    pub fn sum<F>(&self, mut x: usize, mut f: F) -> usize
+    where
+        F: FnMut(usize),
+    {
+        while let Some(p) = self.parent(x) {
+            f(x);
+            x = p;
+        }
+        x
+    }
+    pub fn size(&self, x: usize) -> usize {
+        assert!(x < self.p.len());
+        let r = self.root(x);
+        (-self.p[r]) as usize
+    }
+}
