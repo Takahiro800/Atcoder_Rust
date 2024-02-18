d44975168b9435a6f5f628b9011f1eebb1f82f06
taemath <hirotaka081630@gmail.com>
2024-02-19 04:44:46 +0900

    refactor(abc341c): クロージャを使って、グリッド操作

--- abc/abc341/src/bin/c.rs
+++ abc/abc341/src/bin/c.rs
@@ -1,43 +1,43 @@
 #![allow(non_snake_case)]
-use proconio::{input, marker::Chars};
+use proconio::{input, marker::Bytes};
 
 fn main() {
     input! {
         H: usize,
         W: usize,
         _N: usize,
-        mut T: Chars,
-        S: [Chars;H]
+        T: Bytes,
+        S: [Bytes;H]
+    };
+
+    let check = |x: usize, y: usize| -> bool {
+        if S[x][y] == b'#' {
+            return false;
+        }
+
+        let dir = [(1, 0), (0, 1), (!0, 0), (0, !0)];
+        let p = "DRUL";
+
+        let mut pos = (x, y);
+        for &t in T.iter() {
+            let k = p.bytes().position(|c| c == t).unwrap();
+            pos.0 = pos.0.wrapping_add(dir[k].0);
+            pos.1 = pos.1.wrapping_add(dir[k].1);
+
+            if S[pos.0][pos.1] == b'#' {
+                return false;
+            }
+        }
+        true
     };
 
     let mut ans = 0;
 
     for i in 0..H {
-        'outer: for j in 0..W {
-            if S[i][j] == '#' {
-                continue;
+        for j in 0..W {
+            if check(i, j) {
+                ans += 1;
             }
-
-            let mut k = i;
-            let mut l = j;
-
-            for c in T.iter() {
-                (k, l) = match c {
-                    'R' => (k, l + 1),
-                    'L' => (k, l.wrapping_add(!0)),
-                    'U' => (k.wrapping_add(!0), l),
-                    'D' => (k + 1, l),
-                    _ => unreachable!(),
-                };
-
-                if k >= H || k == 0 || l >= W || l == 0 {
-                    continue 'outer;
-                }
-                if S[k][l] == '#' {
-                    continue 'outer;
-                }
-            }
-            ans += 1;
         }
     }
     println!("{}", ans);
