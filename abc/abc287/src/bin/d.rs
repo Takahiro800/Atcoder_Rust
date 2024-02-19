ecf1704711c2dc57f72043c4bdceeba3fe890a31
taemath <hirotaka081630@gmail.com>
2024-02-20 04:16:27 +0900

    feat(abc287d): stringの前と後ろから累積和

--- abc/abc287/src/bin/d.rs
+++ abc/abc287/src/bin/d.rs
@@ -1,8 +1,37 @@
 #![allow(non_snake_case)]
-// use itertools::*;
-use proconio::input;
-// use superslice::*;
+use proconio::{input, marker::Bytes};
 
 fn main() {
-    input!{};
+    input! {
+        mut S: Bytes,
+        mut T: Bytes,
+    };
+
+    let len = T.len() + 1;
+    let mut prefix = vec![false; len];
+    let mut suffix = vec![false; len];
+
+    prefix[0] = true;
+    suffix[0] = true;
+
+    for i in 1..=T.len() {
+        if prefix[i - 1] {
+            if S[i - 1] == b'?' || T[i - 1] == b'?' || S[i - 1] == T[i - 1] {
+                prefix[i] = true;
+            }
+        }
+
+        if suffix[i - 1] {
+            if S[S.len() - i] == b'?' || T[T.len() - i] == b'?' || S[S.len() - i] == T[T.len() - i]
+            {
+                suffix[i] = true;
+            }
+        }
+    }
+    suffix.reverse();
+
+    for (&p, &s) in suffix.iter().zip(prefix.iter()) {
+        let ans = p && s;
+        println!("{}", if ans { "Yes" } else { "No" });
+    }
 }
