8250a39f32e25e672aba68bbfaee3a2146c1e49a
taemath <hirotaka081630@gmail.com>
2024-02-20 04:22:20 +0900

    refactor(abc287d): take_while: イテレータを評価する。

--- abc/abc287/src/bin/d.rs
+++ abc/abc287/src/bin/d.rs
@@ -7,31 +7,27 @@ fn main() {
         mut T: Bytes,
     };
 
-    let len = T.len() + 1;
-    let mut prefix = vec![false; len];
-    let mut suffix = vec![false; len];
+    let mut ans = vec![];
+    for _ in 0..2 {
+        let cnt = S
+            .iter()
+            .zip(T.iter())
+            .take_while(|(&s, &t)| s == t || s == b'?' || t == b'?')
+            .count();
+        ans.push(cnt);
 
-    prefix[0] = true;
-    suffix[0] = true;
-
-    for i in 1..=T.len() {
-        if prefix[i - 1] {
-            if S[i - 1] == b'?' || T[i - 1] == b'?' || S[i - 1] == T[i - 1] {
-                prefix[i] = true;
-            }
-        }
-
-        if suffix[i - 1] {
-            if S[S.len() - i] == b'?' || T[T.len() - i] == b'?' || S[S.len() - i] == T[T.len() - i]
-            {
-                suffix[i] = true;
-            }
-        }
+        S.reverse();
+        T.reverse();
     }
-    suffix.reverse();
 
-    for (&p, &s) in suffix.iter().zip(prefix.iter()) {
-        let ans = p && s;
-        println!("{}", if ans { "Yes" } else { "No" });
+    for x in 0..=T.len() {
+        println!(
+            "{}",
+            if x <= ans[0] && T.len() - x <= ans[1] {
+                "Yes"
+            } else {
+                "No"
+            }
+        );
     }
 }
