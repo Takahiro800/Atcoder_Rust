dc8bd9a0693f4dc355fd8419af7673f37a5357bc
taemath <hirotaka081630@gmail.com>
2024-02-21 05:31:51 +0900

    feat(abc248c): 数列の構築方法の組み合わせを求める。DPを使う。

--- abc/abc248/src/bin/c.rs
+++ abc/abc248/src/bin/c.rs
@@ -2,5 +2,26 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        M: usize,
+        K: usize
+    };
+
+    const MOD: usize = 998_244_353;
+    let mut dp = vec![1];
+
+    for _ in 0..N {
+        let mut next = vec![0; dp.len() + M];
+        for (i, dp) in dp.iter().enumerate() {
+            for j in 1..=M {
+                next[i + j] += dp;
+            }
+        }
+        dp = next;
+        dp.iter_mut().for_each(|dp| *dp %= MOD)
+    }
+    dp.truncate(K + 1);
+    let ans = dp.iter().sum::<usize>() % MOD;
+    println!("{}", ans);
 }
