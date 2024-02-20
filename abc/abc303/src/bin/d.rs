590fa31dd3d9ac462f9bd4e0b31a1f0172201999
taemath <hirotaka081630@gmail.com>
2024-02-21 08:11:18 +0900

    feat(abc303d): 前の状態の候補が複数存在するのでDPを考える。

--- abc/abc303/src/bin/d.rs
+++ abc/abc303/src/bin/d.rs
@@ -1,8 +1,32 @@
 #![allow(non_snake_case)]
-// use itertools::*;
-use proconio::input;
-// use superslice::*;
+use proconio::{input, marker::Bytes};
 
 fn main() {
-    input!{};
+    input! {
+        X: usize,
+        Y: usize,
+        Z: usize,
+        S: Bytes
+    };
+
+    let mut on = vec![0; S.len() + 1];
+    let mut off = vec![0; S.len() + 1];
+
+    on[0] = Z;
+
+    for i in 0..S.len() {
+        match S[i] {
+            b'a' => {
+                off[i + 1] = (on[i] + Z + X).min(off[i] + X);
+                on[i + 1] = (on[i] + Y).min(off[i] + Z + Y);
+            }
+            b'A' => {
+                on[i + 1] = (on[i] + X).min(off[i] + Z + X);
+                off[i + 1] = (on[i] + Z + Y).min(off[i] + Y);
+            }
+            _ => unreachable!(),
+        }
+    }
+    let ans = on[S.len()].min(off[S.len()]);
+    println!("{}", ans);
 }
