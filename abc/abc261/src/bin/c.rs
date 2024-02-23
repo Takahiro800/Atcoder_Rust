2cbcb05828eb70ef351c5fdd579adbb1a871968d
taemath <hirotaka081630@gmail.com>
2024-02-23 09:58:25 +0900

    feat(abc261c): solve it

--- abc/abc261/src/bin/c.rs
+++ abc/abc261/src/bin/c.rs
@@ -1,6 +1,29 @@
 #![allow(non_snake_case)]
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize
+    };
+
+    let mut map = HashMap::new();
+
+    for _ in 0..N {
+        input! {
+            S: String
+        }
+
+        match map.get(&S) {
+            Some(c) => {
+                println!("{}({})", S, c);
+                map.insert(S, c + 1);
+            }
+            None => {
+                println!("{}", S);
+                map.insert(S, 1);
+            }
+        }
+    }
 }
