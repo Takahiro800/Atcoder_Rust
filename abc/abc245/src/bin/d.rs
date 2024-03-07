015c989f64c867b0354a06c4274847fcc1f78510
taemath <hirotaka081630@gmail.com>
2024-03-08 08:40:25 +0900

    feat(abc245d): solve it

--- abc/abc245/src/bin/d.rs
+++ abc/abc245/src/bin/d.rs
@@ -1,8 +1,27 @@
 #![allow(non_snake_case)]
 use itertools::*;
 use proconio::input;
-use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        M:usize,
+        mut A: [isize;N+1],
+        mut C: [isize; N+M+1]
+    };
+
+    A.reverse();
+    C.reverse();
+    let mut ans = vec![];
+
+    for i in 0..=M {
+        let q = C[i] / A[0];
+        ans.push(q);
+
+        for (c, a) in C[i..].iter_mut().zip(A.iter()) {
+            *c -= a * q;
+        }
+    }
+    ans.reverse();
+    println!("{}", ans.iter().join(" "));
 }
