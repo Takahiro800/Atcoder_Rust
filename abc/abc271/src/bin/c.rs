ce39a2b7d894e86396513997c588bfedca66ceb4
taemath <hirotaka081630@gmail.com>
2024-03-21 06:22:21 +0900

    feat(abc271d): 不要なコメントを削除

--- abc/abc271/src/bin/c.rs
+++ abc/abc271/src/bin/c.rs
@@ -25,25 +25,20 @@ fn main() {
             break;
         }
     }
-    // println!(
-    //     "ans: {}, left: {}, right: {}, count: {}",
-    //     ans, left, right, count
-    // );
 
     while left < right || count > 1 {
         if left < N && ans + 1 == A[left + 1] {
-            // println!("left- ans: {}, left: {}, right: {}", ans, left, right);
             ans += 1;
+
             let pre = left;
             left = A.lower_bound(&(ans + 1)) - 1;
             count += left - pre - 1;
         } else if right > 1 && right + count > left + 1 {
-            let mut rem = 2;
-            let t = count.min(2);
-            // println!("right- ans: {}, left: {}, right: {}", ans, left, right);
             ans += 1;
-            rem -= t;
+
+            let t = count.min(2);
             count -= t;
+            let rem = 2 - t;
             right -= rem;
         } else {
             break;
