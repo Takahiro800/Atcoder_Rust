fefede4f9be2bb24f8231f9c19443bffb12af264
taemath <hirotaka081630@gmail.com>
2024-03-21 09:22:49 +0900

    feat(abc271c): 二分探索による解法
    どれくらい買えるか？ではなく、X冊買えるか？から決めうちする

--- abc/abc271/src/bin/c.rs
+++ abc/abc271/src/bin/c.rs
@@ -7,42 +7,29 @@ fn main() {
         N: usize,
         mut A: [usize;N]
     };
-    A.push(0);
     A.sort();
-    let mut ans = 0;
+    A.dedup();
+
     let mut left = 0;
-    let mut right = N;
+    let mut right = N + 1;
 
-    let mut count = 0;
+    while right > left + 1 {
+        let mid = (left + right) / 2;
+        let have = A.lower_bound(&(mid + 1));
 
-    for i in A.iter().skip(1) {
-        if ans + 1 == *i {
-            ans += 1;
-            let pre = left;
-            left = A.lower_bound(&(ans + 1)) - 1;
-            count += left - pre - 1;
+        let count = N - have;
+        let mut need = 0;
+
+        if mid > have {
+            need = (mid - have) * 2;
+        };
+
+        if count >= need {
+            left = mid;
         } else {
-            break;
+            right = mid;
         }
     }
 
-    while left < right || count > 1 {
-        if left < N && ans + 1 == A[left + 1] {
-            ans += 1;
-
-            let pre = left;
-            left = A.lower_bound(&(ans + 1)) - 1;
-            count += left - pre - 1;
-        } else if right > 1 && right + count > left + 1 {
-            ans += 1;
-
-            let t = count.min(2);
-            count -= t;
-            let rem = 2 - t;
-            right -= rem;
-        } else {
-            break;
-        }
-    }
-    println!("{}", ans);
+    println!("{}", left);
 }
