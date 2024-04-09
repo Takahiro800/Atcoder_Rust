fe204e72c8df1d94f70c716b2e709e8131169d03
taemath <hirotaka081630@gmail.com>
2024-04-10 06:01:43 +0900

    feat(abc321a~b) solve it

--- abc/abc321/src/bin/a.rs
+++ abc/abc321/src/bin/a.rs
@@ -1,6 +1,16 @@
 #![allow(non_snake_case)]
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input!{};
+    input! {
+        N: Chars,
+    };
+
+    for w in N.windows(2) {
+        if w[0] <= w[1] {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
--- abc/abc321/src/bin/b.rs
+++ abc/abc321/src/bin/b.rs
@@ -2,5 +2,33 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        X: usize,
+        mut A: [usize; N-1],
+    };
+
+    A.sort();
+
+    let min = A[0];
+    let max = A[N - 2];
+    let current: usize = A.iter().sum::<usize>() - (min + max);
+
+    if current > X {
+        println!("0");
+        return;
+    }
+
+    let diff = X - current;
+
+    let ans;
+    if diff <= min {
+        ans = 0;
+    } else if min < diff && diff <= max {
+        ans = diff as isize;
+    } else {
+        ans = -1;
+    }
+
+    println!("{}", ans);
 }
