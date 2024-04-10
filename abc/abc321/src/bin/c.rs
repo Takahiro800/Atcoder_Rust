b1b6e96cabd44747641d7fc8ae08d065c4c2e8da
taemath <hirotaka081630@gmail.com>
2024-04-11 05:09:24 +0900

    feat(abc321c) solve it!

--- abc/abc321/src/bin/c.rs
+++ abc/abc321/src/bin/c.rs
@@ -1,6 +1,21 @@
 #![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        K: usize,
+    };
+
+    let numbers = (2..1 << 10)
+        .map(|bs| {
+            (0..10)
+                .rev()
+                .filter(|i| bs >> i & 1 == 1)
+                .fold(0_u64, |acc, x| acc * 10 + x)
+        })
+        .sorted()
+        .collect_vec();
+
+    println!("{}", numbers[K - 1])
 }
