0bafe7bdfcf904c61e26e3451995491bca155809
taemath <hirotaka081630@gmail.com>
2024-04-15 22:03:07 +0900

    feat(abc330d) solve
    iproductを使う
    図形的に考察する

--- abc/abc330/src/bin/d.rs
+++ abc/abc330/src/bin/d.rs
@@ -1,8 +1,24 @@
 #![allow(non_snake_case)]
-use itertools::*;
+use itertools::{iproduct, Itertools};
 use proconio::input;
-use superslice::*;
+use proconio::marker::Chars;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        S: [Chars; N],
+    }
+
+    let a = (0..N)
+        .map(|i| S[i].iter().copied().filter(|&c| c == 'o').count())
+        .collect_vec();
+    let b = (0..N)
+        .map(|j| (0..N).map(|i| S[i][j]).filter(|&c| c == 'o').count())
+        .collect_vec();
+
+    let ans = iproduct!(0..N, 0..N)
+        .filter(|&(i, j)| S[i][j] == 'o')
+        .map(|(i, j)| (a[i] - 1) * (b[j] - 1))
+        .sum::<usize>();
+    println!("{}", ans);
 }
