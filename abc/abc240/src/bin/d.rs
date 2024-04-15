469dbd1623efec0f4ae95eb36d1500c8433e2014
taemath <hirotaka081630@gmail.com>
2024-04-15 22:31:51 +0900

    feat(abc240d) solve
    - stackとしてVecを使う
    - anotationが必要だったり、型推論で不要だったりがあるので、Vec::<>::new()を使うと良いかも

--- abc/abc240/src/bin/d.rs
+++ abc/abc240/src/bin/d.rs
@@ -1,8 +1,29 @@
 #![allow(non_snake_case)]
-// use itertools::*;
 use proconio::input;
-// use superslice::*;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        A: [usize;N]
+    };
+
+    let mut stack = Vec::<(usize, usize)>::new();
+    for &x in A.iter() {
+        let count = if stack.last().map(|&(y, _)| y) == Some(x) {
+            stack.last().unwrap().1 + 1
+        } else {
+            1
+        };
+        stack.push((x, count));
+
+        while stack.last().filter(|&(x, count)| x == count).is_some() {
+            let count = stack.last().unwrap().1;
+            for _ in 0..count {
+                stack.pop().unwrap();
+            }
+        }
+
+        let ans = stack.len();
+        println!("{}", ans);
+    }
 }
