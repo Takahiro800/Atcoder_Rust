8e2375ecda51b9991114f675e66881846e61403f
taemath <hirotaka081630@gmail.com>
2024-04-15 20:57:07 +0900

    feat(abc319a~b) solve

--- abc/abc319/src/bin/a.rs
+++ abc/abc319/src/bin/a.rs
@@ -2,5 +2,22 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        S: String
+    };
+
+    let ans = match S.as_str() {
+        "tourist" => 3858,
+        "ksun48" => 3679,
+        "Benq" => 3658,
+        "Um_nik" => 3648,
+        "apiad" => 3638,
+        "Stonefeang" => 3630,
+        "ecnerwala" => 3613,
+        "mnbvmar" => 3555,
+        "newbiedmy" => 3516,
+        "semiexp" => 3481,
+        _ => unreachable!(),
+    };
+    println!("{}", ans);
 }
--- abc/abc319/src/bin/b.rs
+++ abc/abc319/src/bin/b.rs
@@ -1,6 +1,24 @@
 #![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize
+    };
+
+    let mut ans = vec![];
+
+    for i in 0..=N {
+        if i == 0 {
+            ans.push('1');
+        } else {
+            let j = (1..=9)
+                .filter(|j| N % j == 0 && i % (N / j) == 0)
+                .min()
+                .map_or('-', |j| (j as u8 + b'0') as char);
+            ans.push(j);
+        }
+    }
+    println!("{}", ans.iter().join(""));
 }
