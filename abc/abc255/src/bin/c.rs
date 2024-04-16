9e75bfdf707677632f022adad999a7b65ee86684
taemath <hirotaka081630@gmail.com>
2024-04-16 21:49:57 +0900

    feat(abc255c) solve by myself

--- abc/abc255/src/bin/c.rs
+++ abc/abc255/src/bin/c.rs
@@ -2,5 +2,29 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        X: isize,
+        A: isize,
+        D: isize,
+        N: usize
+    };
+
+    let min = A.min(A + D * (N as isize - 1));
+    let max = A.max(A + D * (N as isize - 1));
+
+    let ans;
+
+    if X <= min {
+        ans = min - X;
+    } else if X >= max {
+        ans = X - max;
+    } else {
+        let m = (X.abs_diff(A)) as isize / D.abs();
+        ans = (m - 1..=m + 1)
+            .map(|p| X.abs_diff(A + D * p) as isize)
+            .min()
+            .unwrap();
+    }
+
+    println!("{}", ans);
 }
