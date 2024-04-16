3d8433dd9c1368d6f13fb37950c2aed6593e6ea6
taemath <hirotaka081630@gmail.com>
2024-04-16 21:49:38 +0900

    feat(abc292d) petgrapthを使ったUnionFind

--- abc/abc292/src/bin/d.rs
+++ abc/abc292/src/bin/d.rs
@@ -1,8 +1,21 @@
 #![allow(non_snake_case)]
-// use itertools::*;
-use proconio::input;
-// use superslice::*;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        M: usize,
+        UV: [(Usize1,Usize1);M]
+    };
+
+    let mut uf = UnionFind::new(N + 1);
+    for &(i, j) in UV.iter() {
+        if !uf.union(i, j) {
+            uf.union(i, N);
+        }
+    }
+
+    let ans = N == M && (0..N).all(|i| uf.equiv(i, N));
+    println!("{}", if ans { "Yes" } else { "No" });
 }
