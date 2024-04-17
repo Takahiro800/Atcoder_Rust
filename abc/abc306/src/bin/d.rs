0fd996fe91b04ee76ce6285fae7ef08c6822b788
taemath <hirotaka081630@gmail.com>
2024-04-17 21:33:26 +0900

    feat(abc309d) solve it by DP

---
+++ abc/abc306/src/bin/d.rs
@@ -0,0 +1,26 @@
+#![allow(non_snake_case)]
+use proconio::input;
+
+fn main() {
+    input! {
+        N: usize,
+        XY: [(usize, isize);N]
+    };
+
+    let mut a: isize = 0;
+    let mut b: isize = 0;
+
+    for &(x, y) in XY.iter() {
+        match x {
+            0 => {
+                a = a.max(a.max(b) + y);
+            }
+            1 => {
+                b = b.max(a + y);
+            }
+            _ => unreachable!(),
+        }
+    }
+    let ans = a.max(b);
+    println!("{}", ans);
+}
