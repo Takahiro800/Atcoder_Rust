0e8dfbe017b20c54f1de764b6b7bf768fe7c28e4
taemath <hirotaka081630@gmail.com>
2024-04-20 23:14:52 +0900

    feat(typical90-04) sum of grid

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,6 +1,27 @@
 #![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        H: usize,
+        W: usize,
+        mut A: [[usize;W];H]
+    };
+
+    let sum_H: Vec<usize> = A.iter().map(|a| a.iter().sum()).collect();
+    let sum_W: Vec<usize> = (0..W).map(|j| A.iter().map(|row| row[j]).sum()).collect();
+
+    let mut ans = vec![vec![0; W]; H];
+
+    for (i, h) in sum_H.iter().enumerate() {
+        for (j, w) in sum_W.iter().enumerate() {
+            let sum = h + w - A[i][j];
+            ans[i][j] = sum;
+        }
+    }
+
+    for a in ans.iter() {
+        println!("{}", a.iter().join(" "));
+    }
 }
