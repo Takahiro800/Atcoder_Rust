31ab3245cb7e93984aa1627d2e86144ab61d6426
taemath <hirotaka081630@gmail.com>
2024-04-21 23:25:16 +0900

    feat(typical90-12) 連結判定なので Union Find

--- typical90/src/bin/012.rs
+++ typical90/src/bin/012.rs
@@ -1,6 +1,57 @@
 #![allow(non_snake_case)]
-use proconio::input;
+use petgraph::unionfind::UnionFind;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    input!{};
+    input! {
+        H: usize,
+        W: usize,
+        Q: usize,
+    };
+
+    let mut grid = vec![vec![false; W]; H];
+    let mut uf = UnionFind::new(H * W);
+    let dir = [(1, 0), (0, 1), (!0, 0), (0, !0)];
+
+    for _ in 0..Q {
+        input! { q: usize };
+
+        match q {
+            1 => {
+                input! {
+                    y: Usize1,
+                    x: Usize1,
+                }
+                grid[y][x] = true;
+
+                for (dx, dy) in dir {
+                    let p = x.wrapping_add(dx);
+                    let q = y.wrapping_add(dy);
+
+                    if p < W && q < H && grid[q][p] {
+                        uf.union(q * W + p, y * W + x);
+                    }
+                }
+            }
+            2 => {
+                input! {
+                    ya: Usize1,
+                    xa: Usize1,
+                    yb: Usize1,
+                    xb: Usize1,
+                }
+
+                if !(grid[ya][xa] && grid[yb][xb]) {
+                    println!("No");
+                } else {
+                    if uf.equiv(ya * W + xa, yb * W + xb) {
+                        println!("Yes")
+                    } else {
+                        println!("No");
+                    }
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
 }
