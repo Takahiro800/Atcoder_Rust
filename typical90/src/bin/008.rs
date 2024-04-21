370f1e5089ec53a7b2796e10b95b2391c2f0e320
taemath <hirotaka081630@gmail.com>
2024-04-21 21:48:38 +0900

    feat(typical90-08) DP

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,6 +1,28 @@
 #![allow(non_snake_case)]
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input!{};
+    input! {
+        _N: usize,
+        S: Chars,
+    };
+
+    let MOD = 10_usize.pow(9) + 7;
+
+    let mut v = vec![0; "atcoder".len()];
+    let atcoder: Vec<char> = "atcoder".chars().collect();
+
+    for &s in S.iter() {
+        if let Some(i) = atcoder.iter().position(|&c| c == s) {
+            if i == 0 {
+                v[i] += 1;
+            } else {
+                v[i] += v[i - 1];
+                v[i] %= MOD
+            }
+        }
+    }
+
+    let ans = v.last().unwrap();
+    println!("{}", ans % MOD);
 }
