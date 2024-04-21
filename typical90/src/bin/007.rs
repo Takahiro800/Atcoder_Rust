ecd7f081e3b36e4461990a6fca62d59a47b3dc8c
taemath <hirotaka081630@gmail.com>
2024-04-21 21:48:10 +0900

    feat(typical90-07) lower_bound
    引数以上の要素が存在しない場合、iter().len()を返すので注意

--- typical90/src/bin/007.rs
+++ typical90/src/bin/007.rs
@@ -1,6 +1,27 @@
 #![allow(non_snake_case)]
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        mut A: [usize; N],
+        Q: usize,
+        B: [usize;Q]
+    };
+
+    A.sort();
+
+    for &b in B.iter() {
+        let mut p = A.lower_bound(&b);
+        if p == N {
+            p -= 1;
+        }
+        let mut diff = A[p].abs_diff(b);
+
+        if p > 0 {
+            diff = diff.min(A[p - 1].abs_diff(b));
+        }
+        println!("{}", diff);
+    }
 }
