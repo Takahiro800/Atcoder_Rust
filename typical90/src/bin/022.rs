54be6a787ab9bbdddb72c43106e9591ac967c4a1
taemath <hirotaka081630@gmail.com>
2024-04-22 21:32:41 +0900

    feat(typical90-22) GCD by num-integer

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,6 +1,15 @@
 #![allow(non_snake_case)]
+use num_integer::Integer;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        A: usize,
+        B: usize,
+        C: usize,
+    };
+
+    let gcd = A.gcd(&B.gcd(&C));
+    let ans = (A + B + C) / gcd - 3;
+    println!("{}", ans);
 }
