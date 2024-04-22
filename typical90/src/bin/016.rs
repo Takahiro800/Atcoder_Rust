ba09be3a1e968e2934a3aac89142b505700f19c4
taemath <hirotaka081630@gmail.com>
2024-04-22 20:59:17 +0900

    feat(typical90-16)

--- typical90/src/bin/016.rs
+++ typical90/src/bin/016.rs
@@ -1,6 +1,25 @@
 #![allow(non_snake_case)]
+use itertools::iproduct;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        a: usize,
+        b: usize,
+        c: usize
+    };
+
+    let mut ans = std::usize::MAX;
+
+    for (i, j) in iproduct!(0..10000, 0..10000) {
+        let sum = a * i + b * j;
+
+        if sum <= N && (N - sum) % c == 0 {
+            let k = (N - sum) / c;
+            ans = ans.min(i + j + k);
+        }
+    }
+
+    println!("{}", ans);
 }
