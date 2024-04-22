262f66f40f097b22c33b977a3cf1185e8a547557
taemath <hirotaka081630@gmail.com>
2024-04-22 20:59:42 +0900

    feat(typical90-20) u64.pow(u32)

--- typical90/src/bin/020.rs
+++ typical90/src/bin/020.rs
@@ -1,6 +1,15 @@
 #![allow(non_snake_case)]
+use std::u64;
+
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        a: u64,
+        b: u32,
+        c: u64,
+    };
+
+    let ans = a < c.pow(b);
+    println!("{}", if ans { "Yes" } else { "No" });
 }
