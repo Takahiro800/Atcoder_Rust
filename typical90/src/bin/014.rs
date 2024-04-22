cf8728f300315f4208a1c7686bab88668eb5a85e
taemath <hirotaka081630@gmail.com>
2024-04-22 20:58:54 +0900

    feat(typical90-14)

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -2,5 +2,20 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        mut A: [usize;N],
+        mut B: [usize;N]
+    };
+
+    A.sort();
+    B.sort();
+
+    let mut ans = 0;
+
+    for (&a, &b) in A.iter().zip(B.iter()) {
+        ans += a.abs_diff(b);
+    }
+
+    println!("{}", ans);
 }
