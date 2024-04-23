0bfab41db94c86bdb189f29cd5f432c8ca4e1ca7
taemath <hirotaka081630@gmail.com>
2024-04-23 22:50:52 +0900

    feat(typical90-27) solve

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,6 +1,18 @@
 #![allow(non_snake_case)]
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        S: [String;N]
+    };
+    let mut set = HashSet::new();
+
+    for (i, s) in S.iter().enumerate() {
+        if set.insert(s) {
+            println!("{}", i + 1);
+        }
+    }
 }
