073085aecb5f640f50082af142d06ea16c6ee282
taemath <hirotaka081630@gmail.com>
2024-04-28 22:35:01 +0900

    refactor(abc351e)

--- abc/abc351/src/bin/e.rs
+++ abc/abc351/src/bin/e.rs
@@ -7,38 +7,26 @@ fn main() {
         XY: [(isize,isize);N]
     };
 
-    let mut evens = vec![];
-    let mut odds = vec![];
+    let (even, odd): (Vec<_>, Vec<_>) = XY.iter().copied().partition(|&(x, y)| (x + y) % 2 == 0);
+    let mut ans = 0_isize;
 
-    for &(x, y) in XY.iter() {
-        let p = x + y;
-        let q = x - y;
+    for points in [even, odd] {
+        let mut points = points
+            .iter()
+            .map(|&(x, y)| (x + y, x - y))
+            .collect::<Vec<_>>();
+        let n = points.len();
 
-        if p % 2 == 0 {
-            evens.push((p, q));
-        } else {
-            odds.push((p, q))
+        points.sort_unstable();
+        for (i, &(x, _)) in points.iter().enumerate() {
+            let coeff = 2 * i as isize + 1 - (n as isize);
+            ans += x * coeff;
         }
-    }
 
-    let mut evens_x = evens.iter().map(|&(x, _)| x).collect::<Vec<_>>();
-    evens_x.sort();
-    let mut evens_y = evens.iter().map(|&(_, y)| y).collect::<Vec<_>>();
-    evens_y.sort();
-    let mut odds_x = odds.iter().map(|&(x, _)| x).collect::<Vec<_>>();
-    odds_x.sort();
-    let mut odds_y = odds.iter().map(|&(_, y)| y).collect::<Vec<_>>();
-    odds_y.sort();
-
-    let p = vec![evens_x, evens_y, odds_x, odds_y];
-
-    let mut ans = 0;
-
-    for set in p.iter() {
-        if set.len() > 1 {
-            for (i, &v) in set.iter().enumerate() {
-                ans += (2 * (i + 1) as isize - set.len() as isize - 1) * v;
-            }
+        points.sort_unstable_by_key(|&(_, y)| y);
+        for (i, &(_, y)) in points.iter().enumerate() {
+            let coeff = 2 * i as isize + 1 - (n as isize);
+            ans += y * coeff;
         }
     }
 
