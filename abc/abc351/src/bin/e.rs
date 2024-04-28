52162263cf7b5ae357a1783f411a85917eb09ebb
taemath <hirotaka081630@gmail.com>
2024-04-28 21:00:39 +0900

    feat(abc351e) TLE on contest

--- abc/abc351/src/bin/e.rs
+++ abc/abc351/src/bin/e.rs
@@ -1,7 +1,28 @@
-
 #![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        XY: [(usize,usize);N]
+    };
+
+    let mut ans = 0;
+
+    for c in (0..N).combinations(2) {
+        let p = XY[c[0]];
+        let q = XY[c[1]];
+
+        let x_diff = p.0.abs_diff(q.0);
+        let y_diff = p.1.abs_diff(q.1);
+
+        if x_diff % 2 != y_diff % 2 {
+            continue;
+        }
+
+        ans += x_diff.max(y_diff);
+    }
+
+    println!("{}", ans);
 }
