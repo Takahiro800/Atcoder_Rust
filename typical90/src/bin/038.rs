c3ff16fce4ff914ea5b858052f7a6279a7a62500
taemath <hirotaka081630@gmail.com>
2024-05-02 22:23:42 +0900

    feat(typical90-38) be careful overflow

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -1,6 +1,29 @@
 #![allow(non_snake_case)]
 use proconio::input;
+use std::mem::swap;
 
 fn main() {
-    input!{};
+    input! {
+        mut A: usize,
+        mut B: usize
+    };
+
+    let gcd = gcd(A, B);
+
+    if A < B {
+        swap(&mut A, &mut B);
+    }
+    if 10_usize.pow(18) / A < B / gcd {
+        println!("Large");
+    } else {
+        println!("{}", A / gcd * B);
+    }
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }
