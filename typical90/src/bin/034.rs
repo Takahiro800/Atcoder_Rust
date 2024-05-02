2e0bc71d0209e87fc55d5783e698df68f88e093d
taemath <hirotaka081630@gmail.com>
2024-05-02 22:23:15 +0900

    feat(typical90-34) 単調増加の尺取法

--- typical90/src/bin/034.rs
+++ typical90/src/bin/034.rs
@@ -1,6 +1,31 @@
 #![allow(non_snake_case)]
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        K: usize,
+        A: [usize;N]
+    };
+
+    let mut j = 0;
+    let mut ans = 0;
+    let mut map = HashMap::new();
+
+    for i in 0..N {
+        while j != N && (map.len() < K || map.contains_key(&A[j])) {
+            *map.entry(&A[j]).or_insert(0) += 1;
+            j += 1;
+        }
+        ans = ans.max(j - i);
+
+        if map[&A[i]] == 1 {
+            map.remove(&A[i]);
+        } else {
+            *map.get_mut(&A[i]).unwrap() -= 1;
+        }
+    }
+
+    println!("{}", ans);
 }
