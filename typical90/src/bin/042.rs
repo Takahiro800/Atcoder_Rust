da2c62041037686e80921787eb0bac507a774d86
taemath <hirotaka081630@gmail.com>
2024-05-03 22:25:19 +0900

    feat(typical90-42) 桁の和に関するDP

--- typical90/src/bin/042.rs
+++ typical90/src/bin/042.rs
@@ -2,5 +2,24 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        K: usize
+    };
+
+    if K % 9 != 0 {
+        println!("{}", 0);
+        return;
+    }
+
+    let mut dp = vec![0; 9];
+    dp[8] = 1;
+
+    for _ in 0..K {
+        let s = dp.iter().sum::<usize>() % 1_000_000_007;
+        dp.rotate_left(1);
+        dp[8] = s;
+    }
+
+    let ans = dp[8];
+    println!("{}", ans);
 }
