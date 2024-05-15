2a15a743e6605ac8849936e675ee8db2dd229641
taemath <hirotaka081630@gmail.com>
2024-05-15 21:17:21 +0900

    feat(abc353ab) solve on contest

--- abc/abc353/src/bin/a.rs
+++ abc/abc353/src/bin/a.rs
@@ -2,5 +2,20 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        H: [usize;N]
+    };
+
+    let h = H[0];
+
+    let mut ans = -1;
+
+    for i in 1..N {
+        if H[i] > h {
+            ans = i as isize + 1;
+            break;
+        }
+    }
+    println!("{}", ans);
 }
--- abc/abc353/src/bin/b.rs
+++ abc/abc353/src/bin/b.rs
@@ -2,5 +2,29 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        K: usize,
+        A: [usize;N]
+    };
+
+    let mut count = 0;
+    let mut empty = K;
+
+    let mut i = 0;
+
+    while i < A.len() {
+        if A[i] <= empty {
+            empty -= A[i];
+            i += 1;
+        } else {
+            count += 1;
+            empty = K;
+        }
+    }
+
+    if empty < K {
+        count += 1;
+    }
+    println!("{}", count);
 }
