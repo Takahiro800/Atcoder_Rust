2033243d9c99c675173829b161717095b3d5beda
taemath <hirotaka081630@gmail.com>
2024-05-15 21:18:37 +0900

    feat(abc353c) upsolve
    可換なのでsortする。
    制約をきちんと確認する。今回の場合、MODより大きくなっても商は１なのでcountすれば良い。

--- abc/abc353/src/bin/c.rs
+++ abc/abc353/src/bin/c.rs
@@ -1,6 +1,30 @@
 #![allow(non_snake_case)]
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        mut A: [usize;N]
+    };
+
+    let MOD = 100_000_000;
+    A.sort();
+    let mut count = 0;
+
+    for (i, &a) in A.iter().enumerate() {
+        let res = MOD - a;
+        let t = A.lower_bound(&res);
+        if t < N {
+            if t <= i {
+                count += N - i - 1;
+            } else {
+                count += N - t;
+            }
+        }
+    }
+    let sum: usize = A.iter().sum();
+    let ans = (sum * (N - 1)) - (10_usize.pow(8) * count);
+
+    println!("{}", ans);
 }
