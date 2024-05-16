560e83bd7ae0eaa32bf9873b6ec931cf3ae5ab32
taemath <hirotaka081630@gmail.com>
2024-05-16 20:57:44 +0900

    feat(tessoku-book) solve b43

--- tessoku-book/src/bin/b43.rs
+++ tessoku-book/src/bin/b43.rs
@@ -1,5 +1,19 @@
-use proconio::input;
+#![allow(non_snake_case)]
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        A: [Usize1; M],
+    }
+
+    let mut ans = vec![M; N];
+    for &a in A.iter() {
+        ans[a] -= 1;
+    }
+
+    for a in ans.iter() {
+        println!("{}", a);
+    }
 }
