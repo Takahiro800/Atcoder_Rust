c9162d3c14d45fa21bd9c34d5a26297d8d594877
taemath <hirotaka081630@gmail.com>
2024-05-27 22:41:43 +0900

    feat(typical90-44) 見かけ上の変化をメモ

--- typical90/src/bin/044.rs
+++ typical90/src/bin/044.rs
@@ -2,5 +2,41 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        Q: usize,
+        mut A: [usize; N],
+    };
+    let mut count = 0;
+
+    for _ in 0..Q {
+        input! {
+            T: usize,
+            mut  x: usize,
+            mut y: usize,
+        }
+
+        match T {
+            1 => {
+                x -= 1;
+                y -= 1;
+
+                x = (x + N - count) % N;
+                y = (y + N - count) % N;
+
+                let temp = A[x];
+                A[x] = A[y];
+                A[y] = temp;
+            }
+            2 => {
+                count += 1;
+            }
+            3 => {
+                x -= 1;
+                let i = (x + N - count) % N;
+                println!("{}", A[i]);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
