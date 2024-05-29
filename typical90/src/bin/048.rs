0ec40ac703099dbb4625abbcadede368e1e4f96e
taemath <hirotaka081630@gmail.com>
2024-05-29 21:52:43 +0900

    feat(typical90-48) upsolve

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -1,6 +1,17 @@
 #![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        K: usize,
+        AB: [(usize, usize); N],
+    };
+    let mut nums = AB.iter().flat_map(|&(a, b)| [a - b, b]).collect_vec();
+    nums.sort();
+    nums.reverse();
+
+    let ans: usize = nums.iter().take(K).sum();
+    println!("{}", ans);
 }
