9e71be12fec5e0828f2a3bf3abc93f0c9153894c
taemath <hirotaka081630@gmail.com>
2024-05-29 22:24:44 +0900

    feat(typical90-50) overflow

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -2,5 +2,27 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: u128,
+        L: u128,
+    };
+    const MOD: u128 = 10_u128.pow(9) + 7;
+
+    let mut ans = 0;
+
+    for i in 0..=N / L {
+        let n = N - (L * i) + i;
+        ans += combination(n, i) % MOD;
+    }
+
+    println!("{}", ans);
+}
+
+fn combination(n: u128, r: u128) -> u128 {
+    let mut result = 1;
+
+    for i in 0..r {
+        result = result * (n - i) / (i + 1);
+    }
+    result
 }
