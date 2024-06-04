c7ed3d24ae728438e6280f81a808e1d4c5c4a31d
taemath <hirotaka081630@gmail.com>
2024-06-04 23:17:47 +0900

    feat(typical90-52) iter().fold()
    MDOの位置に注意

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -2,5 +2,13 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        A: [[usize;6]; N]
+    };
+
+    const MOD: usize = 10_usize.pow(9) + 7;
+    let ans = A.iter().fold(1, |s, a| s * a.iter().sum::<usize>() % MOD);
+
+    println!("{}", ans % MOD);
 }
