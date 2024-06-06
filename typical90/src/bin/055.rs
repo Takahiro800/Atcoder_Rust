184bc59b8d23ec44f2ad23fb4c2a016b094e3903
taemath <hirotaka081630@gmail.com>
2024-06-06 22:53:06 +0900

    feat(typical90) solve 55

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -2,5 +2,25 @@
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        P: usize,
+        Q: usize,
+        mut A: [usize; N],
+    };
+    let mut ans = 0;
+    for i in 0..N {
+        for j in i + 1..N {
+            for k in j + 1..N {
+                for l in k + 1..N {
+                    for m in l + 1..N {
+                        if A[i] * A[j] % P * A[k] % P * A[l] % P * A[m] % P == Q {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
