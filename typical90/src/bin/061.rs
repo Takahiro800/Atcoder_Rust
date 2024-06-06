4f6dad4f8ff52a3ad98694acdbbf2dcf0f25176f
taemath <hirotaka081630@gmail.com>
2024-06-06 22:53:54 +0900

    feat(typical90-61) solve by using VecDeque

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,6 +1,26 @@
 #![allow(non_snake_case)]
+use std::collections::VecDeque;
+
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        Q: usize,
+    };
+
+    let mut set = VecDeque::new();
+
+    for _ in 0..Q {
+        input! {
+            t: usize,
+            x: usize
+        }
+
+        match t {
+            1 => set.push_front(x),
+            2 => set.push_back(x),
+            3 => println!("{}", set.get(x - 1).unwrap()),
+            _ => unreachable!(),
+        }
+    }
 }
