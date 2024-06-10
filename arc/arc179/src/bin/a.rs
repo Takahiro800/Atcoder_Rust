4ffeb23cb06c659b2972e7f9aac3345ad0fbdb25
taemath <hirotaka081630@gmail.com>
2024-06-10 21:43:48 +0900

    feat(arc179a) sovle on contest

--- arc/arc179/src/bin/a.rs
+++ arc/arc179/src/bin/a.rs
@@ -1,6 +1,24 @@
 #![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!{};
+    input! {
+        N: usize,
+        K: isize,
+        mut A: [isize;N]
+    };
+
+    A.sort();
+
+    if K > 0 || (A[0] >= 0) {
+        println!("Yes");
+        println!("{}", A.iter().join(" "));
+    } else if A.iter().map(|&a| a).sum::<isize>() >= K {
+        A.reverse();
+        println!("Yes");
+        println!("{}", A.iter().join(" "));
+    } else {
+        println!("No");
+    };
 }
